291ddffb226e9a455c273aea5819dcbcf1ac5384
Venx84 <chrisx84@live.ca>
2025-09-29 18:19:04 -0400

    Update GpaProvider.php

--- GpaProvider.php
+++ GpaProvider.php
@@ -1,65 +1,24 @@
 <?php
-
 namespace GPA;
-
 use GuzzleHttp\RequestOptions;
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
-
-class GpaProvider extends AbstractProvider
-{
+class GpaProvider extends AbstractProvider {
     public const IDENTIFIER = 'GPA';
-
-    protected $scopes = [
-        'users',
-    ];
-    protected $consent = false;
-
+    protected $scopes = ['users'];
     protected $scopeSeparator = ' ';
-
-    protected function getAuthUrl($state): string
-    {
-        return $this->buildAuthUrlFromBase('https://accounts.gridplay.ca/oauth/authorize', $state);
+    protected function getAuthUrl($state) {
+        return $this->buildAuthUrlFromBase(
+            'https://accounts.gridplay.ca/oauth/authorize',
+            $state
+        );
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getCodeFields($state = null)
-    {
-        $fields = parent::getCodeFields($state);
-
-        if (! $this->consent) {
-            $fields['prompt'] = 'none';
-        }
-
-        return $fields;
+    protected function getTokenUrl() {
+        return 'https://accounts.gridplay.ca/oauth/token';
     }
-
-    /**
-     * Prompt for consent each time or not.
-     *
-     * @return $this
-     */
-    public function withConsent()
-    {
-        $this->consent = true;
-
-        return $this;
-    }
-
-    protected function getTokenUrl(): string
-    {
-        return "https://accounts.gridplay.ca/oauth/token";
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getUserByToken($token)
-    {
+    protected function getUserByToken($token) {
         $response = $this->getHttpClient()->get(
-            'https://accounts.gridplay.ca/api/users',
+            'https://accounts.gridplay.ca/api/user',
             [
                 RequestOptions::HEADERS => [
                     'Authorization' => 'Bearer '.$token,
@@ -69,13 +28,11 @@ class GpaProvider extends AbstractProvider
 
         return json_decode((string) $response->getBody(), true);
     }
-
-    protected function mapUserToObject(array $user)
-    {
-        return (new User)->setRaw($user)->map([
-            'id'       => $user['id'],
-            'name'     => $user['name'],
-            'email'    => $user['email'] ?? null,
+    protected function mapUserToObject(array $user) {
+        return (new User())->setRaw($user)->map([
+            'id'   => $user['id'],
+            'name' => $user['name'],
+            'email' => $user['email']
         ]);
     }
 }
